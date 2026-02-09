6cbc9ffc9c87903e1cba38c54bfc2979b5544746
Mahalakshmi K <mahalakshmikamaraj2007@gmail.com>
2026-02-09 20:53:46 +0530

    Time: 0 ms (100%), Space: 43.4 MB (17.98%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -2,7 +2,7 @@ class Solution {
     public int strStr(String k, String needle) {
         int m=k.length();
         int n=needle.length();
-        for(int i=0;i<m-n;i++){
+        for(int i=0;i<=m-n;i++){
             if(k.substring(i,i+n).equals(needle)){
                 return i;
             }
