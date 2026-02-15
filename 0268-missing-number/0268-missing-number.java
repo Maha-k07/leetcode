87a46569788d19d7dff57a98bc950fc3c6b1a9f4
Mahalakshmi K <mahalakshmikamaraj2007@gmail.com>
2026-02-15 11:17:07 +0530

    Time: 0 ms (100%), Space: 47.6 MB (18.92%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -3,9 +3,9 @@ class Solution {
         int n=nums.length;
         int sum=0;
         int req=n*(n+1)/2;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<nums.length;i++){
             sum+=nums[i];
         }
-    return req-sum;
+        return req-sum;
     }
 }
