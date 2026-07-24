5b4f752892fb04a22e4c4ca38a6df6cd7a590a50
Mahalakshmi K <mahalakshmikamaraj2007@gmail.com>
2026-07-24 09:58:30 +0530

    Time: 3 ms (59.19%), Space: 44.4 MB (56.01%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isPalindrome(String s) {
+        s=s.toLowerCase();
         int left=0;
         int right=s.length()-1;
         while(left<right){
@@ -9,12 +10,12 @@ class Solution {
             while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
                 right--;
             }
-            if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
+            if(s.charAt(left)!=s.charAt(right)){
                 return false;
             }
             left++;
             right--;
         }
-    return true;
+        return true;
     }
 }
